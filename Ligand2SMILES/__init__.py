19590b5c73b2e1d2b753cc2d11cb65598a1b5d38
Pedro Durao <261281137+Pedro-DR-TH@users.noreply.github.com>
2026-07-02 13:06:24 -0500

    Update import statements in __init__.py

--- Ligand2SMILES/__init__.py
+++ Ligand2SMILES/__init__.py
@@ -1,3 +1,3 @@
-from .ligand_lookup import name_to_smiles, search, fuzzy_search, available_names
+from Ligand2SMILES import name_to_smiles, search, fuzzy_search, available_names
 
 __all__ = ["name_to_smiles", "search", "fuzzy_search", "available_names"]
